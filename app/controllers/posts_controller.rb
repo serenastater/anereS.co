60a2ad1623ce08e0ba6e1fe8fed539c395b0235b
Serena Stater <srstater@gmail.com>
2016-06-13 22:46:36 -0400

    finishing posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -16,9 +18,32 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update post_params
+      redirect_to @post, notice: "Shazam! That shit was saved!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
